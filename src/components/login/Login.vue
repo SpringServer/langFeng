2e94728e2d778c74f10f65a8460a707bcff41c64
Spring <531894107@qq.com>
2019-02-18 16:49:17 +0800

    eat first

---
+++ src/components/login/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      login page
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'login',
+  data () {
+    return {
+
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style scoped style="scss">
+</style>
